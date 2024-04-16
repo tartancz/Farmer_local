d2b827e7b3b38e43f333706704ff7250fbcb5981
Jan Růžička <jru@gatema.cz>
2024-04-16 12:30:06 +0200

    created code table

--- src/database/migration/_migration/0001.sql
+++ src/database/migration/_migration/0001.sql
@@ -28,10 +28,39 @@ CREATE TABLE "youtube_video"
     "created"         timestamp DEFAULT CURRENT_TIMESTAMP NOT NULL
 );
 
+CREATE TABLE "code"
+(
+    "id"                           integer PRIMARY KEY,
+    "video_id"                     integer REFERENCES youtube_video (video_id) NOT NULL,
+    "code"                         nvarchar                                    NOT NULL,
+    "how_long_to_process_in_total" float                                       NOT NULL,
+    "code_state_id" primary key REFERENCES code_state (id)                     NOT NULL,
+    "timestamp"                    float                                       NULL,
+    "path_to_frame"                NVARCHAR                                    NULL,
+    "created"                      timestamp DEFAULT CURRENT_TIMESTAMP         NOT NULL
 
+);
+
+CREATE TABLE "code_state"
+(
+    "id"   integer PRIMARY KEY,
+    "name" nvarchar NOT NULL
+);
 
 CREATE TABLE "database_version"
 (
     "VERSION" nvarchar NOT NULL
 );
-INSERT INTO "database_version"(VERSION) VALUES ('0001')
+
+INSERT INTO "code_state"
+    (id, name)
+VALUES (1, 'SUCCESSFULLY_REDEEM'),
+       (2, 'ALREADY_TAKEN'),
+       (3, 'BAD_CODE'),
+       (4, 'EXPIRED'),
+       (5, 'TOO_MANY_REQUESTS'),
+       (6, 'UNKNOWN_ERROR');
+
+
+INSERT INTO "database_version"(VERSION)
+VALUES ('0001');
