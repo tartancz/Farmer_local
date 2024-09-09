34a7b257344d2375bc39f1c810601d45c7cf8e2e
tartan <tartanec@gmail.com>
2024-09-09 16:22:50 +0200

    function for returning next user to redeem code

--- src/database/models/wolt_account.py
+++ src/database/models/wolt_account.py
@@ -50,3 +50,43 @@ class WoltAccountModel(Model):
         if not account_row:
             raise RowDontExistException(f"WoltAccount with account_name {account_name} do not exist")
         return WoltAccount(*account_row)
+
+    def get_credits_this_month(self, account_id: int) -> int:
+        select_query = '''
+            SELECT SUM(value)
+            FROM code
+            WHERE account_id = ? AND activated_at >= datetime('now', 'start of month');
+        '''
+        self.run_sql(select_query, [account_id])
+        return self._cursor.fetchone()[0] or 0
+
+    def get_user_for_redeem(self):
+        select_query = '''
+            SELECT id, account_name, created, max_credits_per_month, priority, working_credentials
+            FROM wolt_account
+            WHERE 
+            priority IS NOT NULL 
+            AND 
+            wolt_account.max_credits_per_month < IFNULL((select SUM(c.value) FROM code c WHERE c.activated_by=wolt_account.account_name AND c.created >= datetime('now', 'start of month')), 0)
+            AND
+            (wolt_account.working_credentials = TRUE OR wolt_account.working_credentials IS NULL)
+            ORDER BY priority 
+            LIMIT 1
+        '''
+        self.run_sql(select_query)
+        account_row = self._cursor.fetchone()
+        if account_row:
+            return WoltAccount(*account_row)
+        select_query = '''
+            SELECT id, account_name, created, max_credits_per_month, priority, working_credentials 
+            FROM wolt_account
+            WHERE 
+            priority IS NOT NULL AND (wolt_account.working_credentials = TRUE OR wolt_account.working_credentials IS NULL)
+            ORDER BY priority
+            LIMIT 1
+        '''
+        self.run_sql(select_query)
+        account_row = self._cursor.fetchone()
+        if account_row:
+            return WoltAccount(*account_row)
+        raise RowDontExistException("No account available for redeeming")
