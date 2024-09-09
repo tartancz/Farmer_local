ebd958871b87537be9340f95949a595d39602f58
tartan <tartanec@gmail.com>
2024-09-09 14:13:33 +0200

    test updated

--- test/mocks/cloud_mock.py
+++ test/mocks/cloud_mock.py
@@ -1,4 +1,12 @@
-def mock_modal():
-    yield '5555'
-    yield '6666'
-    yield '123456789'
+from typing import Generator
+
+from src.cloud_types import CodeType
+from src.video_processor.video_processor import VideoProcessor
+from src.watcher.youtube_api import DetailedVideoFromApi
+
+
+class MockProcessor(VideoProcessor):
+    def get_codes(self, video: DetailedVideoFromApi) -> Generator[CodeType, None, None]:
+        yield {"code": "5555", "timestamp": 2.0, "frame": b"1"}
+        yield {"code": "6666", "timestamp": 2.0, "frame": b"1"}
+        yield {"code": "123456789", "timestamp": 2.0, "frame": b"1"}
